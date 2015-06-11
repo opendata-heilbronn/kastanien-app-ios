8d41178b650057a4b69fbec5e7d01b0fce0713ba
Adrian Stabiszewski <as@grundid.de>
2015-06-11 20:33:16 +0200

    initial

---
+++ KastanienApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KastanienApp
+//
+//  Created by Adrian Stabiszewski on 11.06.15.
+//  Copyright (c) 2015 OpenData Heilbronn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'KastanienApp' do
+  pod 'AFNetworking', '~> 2.5'
+  pod 'SwiftyJSON', '~> 2.1'
+end
